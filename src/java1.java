ff1cad57a960e58f2422fb586897cb1efd99f1a4
madhu8298 <madhukeshwar8@gmail.com>
2023-08-11 11:06:57 +0530

    1st commit

--- src/java1.java
+++ src/java1.java
@@ -2,7 +2,7 @@ public class java1 {
 
     public static void main(String[] args) {
         int[] mad = new int[]{1, 2, 3, 4, 5};
-        String[] sushant = new String[]{"mad", " sush"};
+        String[] sushant = new String[]{"madh", " sush"};
         for (int a:mad)//a is iterator  //for each loop
 
          {
